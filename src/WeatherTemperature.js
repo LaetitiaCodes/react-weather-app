a4aa508bacefb18b662fbc115e73c1e973fbe7f7
Laetitia <88789728+LaetitiaCodes@users.noreply.github.com>
2022-03-19 11:42:40 +0100

    added temp unit converter

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import "./WeatherTemperature.css";
 
 export default function WeatherTemperature(props) {
-  return (
-    <div className="row text-center mb-0">
-      <div className="col-sm-3 mt-4 mb-4" id="temperatureContainer">
-        <span className="Temperature">
-          {" "}
+  const [unit, setUnit] = useState("celsius");
+
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+  if (unit === "celsius") {
+    return (
+      <div className="row text-center mb-0">
+        <div className="col-sm-3 mt-4 mb-4" id="temperatureContainer">
           <span className="cityTemperature" id="cityTemperature">
             {Math.round(props.celsius)}
           </span>
-          <span id="celsius">°C </span>
-          <span id="fahrenheit">|°F</span>
-        </span>
-      </div>{" "}
-    </div>
-  );
+          <span id="celsius">
+            °C{" "}
+            <a href="/" onClick={showFahrenheit}>
+              <span id="fahrenheit">|°F</span>
+            </a>
+          </span>
+        </div>{" "}
+      </div>
+    );
+  } else {
+    return (
+      <div className="row text-center mb-0">
+        <div className="col-sm-3 mt-4 mb-4" id="temperatureContainer">
+          <span className="cityTemperature" id="cityTemperature">
+            {Math.round(fahrenheit())}
+          </span>
+          <span id="celsius">
+            <a href="/" onClick={showCelsius}>
+              {" "}
+              °C{" "}
+            </a>{" "}
+            |°F
+          </span>
+        </div>{" "}
+      </div>
+    );
+  }
 }
