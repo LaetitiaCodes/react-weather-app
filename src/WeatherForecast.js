dda749b05e70cd02f642af3994cef9dc7fc1d9db
Laetitia <88789728+LaetitiaCodes@users.noreply.github.com>
2022-03-20 14:24:43 +0100

    committing progress / fixing css

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -27,7 +27,7 @@ export default function WeatherForecast(props) {
 
   if (loaded) {
     return (
-      <div className="WeatherForecast">
+      <div className="weatherForecast">
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
             if (index < 5) {
